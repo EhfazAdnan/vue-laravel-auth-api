754eaae42e11fd8adc78c9dd2615a786a59e710e
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-19 22:32:27 +0600

    Authentication with JWT

---
+++ app/Http/Controllers/Auth/MeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MeController extends Controller
+{
+    public function __construct(){
+        $this->middleware(['auth:api']);
+    }
+
+    public function __invoke(Request $request){
+        $user = $request->user();
+
+        return response()->json([
+           'email' => $user->email,
+           'name'  => $user->name,
+        ]);
+    }
+}
